dabe1fbc76da5c759b1472b4d7aa09de013d7169
vmaxtacular513 <veronicamaxwell152@gmail.com>
2020-10-14 17:08:46 -0400

    Add local storage for name and focus

--- js/main.js
+++ js/main.js
@@ -4,6 +4,8 @@ const time = document.getElementById('time'),
   name = document.getElementById('name'),
   focus = document.getElementById('focus');
 
+//Options
+const showAmPm = true;
 
 //Show time
 function showTime() {
@@ -19,7 +21,7 @@ function showTime() {
   hour = hour % 12 || 12;
 
   //Output Time
-  time.innerHTML = `${hour}<span>:</span>${addZero(min)}<span>:</span>${addZero(sec)}`;
+  time.innerHTML = `${hour}<span>:</span>${addZero(min)}<span>:</span>${addZero(sec)} ${showAmPm ? amPm : ''}`;
 
   setTimeout(showTime, 1000);
 }
@@ -47,7 +49,62 @@ function setBgGreet() {
   }
 }
 
+//Get Name
+function getName() {
+  if(localStorage.getItem('name') === null) {
+    name.textContent = '[Enter Name]';
+  } else {
+    name.textContent = localStorage.getItem('name');
+  }
+}
+
+//Set Name
+function setName(e) {
+  if(e.type === 'keypress') {
+    //Make sure enter is pressed
+    if(e.which === 13 || e.keyCode === 13) {
+      localStorage.setItem('name', e.target.innerText);
+      //Stop cursur from moving to next line
+      name.blur();
+    }
+  } else {
+    localStorage.setItem('name', e.target.innerText);
+  }
+}
+
+//Get Focus
+function getFocus() {
+  if(localStorage.getItem('focus') === null) {
+    focus.textContent = '[Enter Focus]';
+  } else {
+    focus.textContent = localStorage.getItem('focus');
+  }
+}
+
+//Set Focus
+function setFocus(e) {
+  if(e.type === 'keypress') {
+    //Make sure enter is pressed
+    if(e.which === 13 || e.keyCode === 13) {
+      localStorage.setItem('focus', e.target.innerText);
+      //Stop cursur from moving to next line
+      focus.blur();
+    }
+  } else {
+    localStorage.setItem('focus', e.target.innerText);
+  }
+}
+
+name.addEventListener('keypress', setName);
+name.addEventListener('blur', setName);
+focus.addEventListener('keypress', setFocus);
+focus.addEventListener('blur', setFocus);
+
 
 //Run
 showTime();
 setBgGreet();
+getName();
+getFocus();
+setName();
+setFocus();
